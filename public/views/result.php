596159bceafa11c03ddc3b02b6068c28d6b1c70e
konsylll <konsyl77@gmail.com>
2016-04-19 19:58:57 +0300

    Changed hover color and border for buttons and added hover effect to summoners in result page

--- public/views/result.php
+++ public/views/result.php
@@ -1,15 +1,19 @@
 <div class="row team-100">
-    <div class="col-md-2 col-sm-2 col-xs-2 compressed" ng-click="result.goSummoner(100, $index)" ng-repeat="summoner in summonersTeam100">
-        <img src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{{champions[summoner.championId]}}_0.jpg" class="img-summoner">
-        <p class="nickname">{{summoner.summonerName}}</p>
-        <p class="grade {{summoner.highestGrade}}">{{summoner.highestGrade}}</p>
+    <div class="col-md-2 col-sm-2 col-xs-2 compressed" ng-repeat="summoner in summonersTeam100">
+        <div class="flow-summoner">
+            <img class="img-summoner" src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{{champions[summoner.championId]}}_0.jpg" ng-click="result.goSummoner(100, $index)">
+            <p class="nickname">{{summoner.summonerName}}</p>
+            <p class="grade {{summoner.highestGrade}}">{{summoner.highestGrade}}</p>
+        </div>
     </div>
 </div>
 <div class="row versus text-center">VS</div>
 <div class="row team-200">
-    <div class="col-md-2 col-sm-2 col-xs-2 compressed" ng-click="result.goSummoner(200, $index)" ng-repeat="summoner in summonersTeam200">
-        <img src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{{champions[summoner.championId]}}_0.jpg" class="img-summoner">
-        <p class="nickname">{{summoner.summonerName}}</p>
-        <p class="grade {{summoner.highestGrade}}">{{summoner.highestGrade}}</p>
+    <div class="col-md-2 col-sm-2 col-xs-2 compressed" ng-repeat="summoner in summonersTeam200">
+        <div class="flow-summoner">
+            <img class="img-summoner" src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{{champions[summoner.championId]}}_0.jpg" ng-click="result.goSummoner(200, $index)">
+            <p class="nickname">{{summoner.summonerName}}</p>
+            <p class="grade {{summoner.highestGrade}}">{{summoner.highestGrade}}</p>
+        </div>
     </div>
 </div>
