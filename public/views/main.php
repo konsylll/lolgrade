2ef3189b2a2d17c3c302a35b28d27819ba17f386
konsylll <konsyl77@gmail.com>
2016-05-02 01:12:19 +0300

    structurized sass and change the size of images on the main page

--- public/views/main.php
+++ public/views/main.php
@@ -9,13 +9,13 @@
                 The Application shows the highest grade every summoner in both teams got by playing the current champ
             </div>
             <div class = "col-md-5">
-                <img class="img-summoner" src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Riven_0.jpg">
-                <p class="nickname">Nickname</p>
-                <p class="grade">B+</p>
-                <div class="shadowed"></div>
-                <p class="pts GOLD">59</p>
-                <p class="division GOLD">V</p>
-                <img class="rank" src="../img/Tiers/GOLD.png">
+                <img class="img-summoner-stable" src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Riven_0.jpg">
+                <p class="nickname-stable">Nickname</p>
+                <p class="grade-stable">B+</p>
+                <div class="shadowed-stable"></div>
+                <p class="pts-stable GOLD">59</p>
+                <p class="division-stable GOLD">V</p>
+                <img class="rank-stable" src="../img/Tiers/GOLD.png">
             </div>
         </div>
     </div>
@@ -25,13 +25,13 @@
                 "NG" means that summoner still hasn't played games by this champions from the grade system has been released.
             </div>
             <div class = "col-md-5 pull-right">
-                <img class="img-summoner" src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Garen_0.jpg">
-                <p class="nickname">Nickname</p>
-                <p class="grade">NG</p>
-                <div class="shadowed"></div>
-                <p class="pts BRONZE">38</p>
-                <p class="division BRONZE">III</p>
-                <img class="rank" src="../img/Tiers/BRONZE.png">
+                <img class="img-summoner-stable" src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Garen_0.jpg">
+                <p class="nickname-stable">Nickname</p>
+                <p class="grade-stable">NG</p>
+                <div class="shadowed-stable"></div>
+                <p class="pts-stable BRONZE">38</p>
+                <p class="division-stable BRONZE">III</p>
+                <img class="rank-stable" src="../img/Tiers/BRONZE.png">
             </div>
         </div>
     </div>
@@ -41,8 +41,8 @@
                 You can also see how summoners performed on each champion they ever played just clicking on summoner's picture in result set.
             </div>
             <div class = "col-md-5 pull-right">
-                <img class="icon-summoner" src="http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/Vayne.png">
-                <p class="icon-grade">C+</p>
+                <img class="icon-summoner-stable" src="http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/Vayne.png">
+                <p class="icon-grade-stable">C+</p>
             </div>
         </div>
     </div>
